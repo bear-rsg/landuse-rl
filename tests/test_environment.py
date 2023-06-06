30ba36ec4d8c346ab7f95eee32e19a5e18d34c78
Thomas Kappas (Advanced Research Computing) <t.kappas@bham.ac.uk>
2023-06-06 15:52:42 +0100

    Split MyTestCase into its own module

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -2,33 +2,8 @@ import unittest
 import numpy as np
 import pandas as pd
 
+from tests.mytestcase import MyTestCase
 from src.environment import Environment
-
-
-class MyTestCase(unittest.TestCase):
-    def expectEqual(self, first, second, msg=None):
-        with self.subTest():
-            self.assertEqual(first, second, msg)
-
-    def expectSequenceEqual(self, first, second, msg=None):
-        with self.subTest():
-            self.assertSequenceEqual(first, second, msg)
-
-    def expectAlmostEqual(self, first, second, places=7, msg=None, delta=None):
-        with self.subTest():
-            self.assertAlmostEqual(first, second, places, msg, delta)
-
-    def expectTrue(self, expr, msg=None):
-        with self.subTest():
-            self.assertTrue(expr, msg)
-
-    def expectFalse(self, expr, msg=None):
-        with self.subTest():
-            self.assertFalse(expr, msg)
-
-    def expectRaises(self, exception, callable, *args, **kwds):
-        with self.subTest():
-            self.assertRaises(exception, callable, *args, **kwds)
 
 
 class TestVariables(MyTestCase):
