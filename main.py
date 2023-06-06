d086bdd7266f617af258a2b0f477210af6049567
Thomas Kappas (Advanced Research Computing) <t.kappas@bham.ac.uk>
2023-06-06 20:04:59 +0100

    Move ReplayBuffer into its own module

--- main.py
+++ main.py
@@ -6,35 +6,9 @@ import logging
 import sys
 import random
 import numpy as np
-from collections import namedtuple, deque
 
 from src.dqn import DQN
-
-
-class ReplayBuffer:
-    def __init__(self, device, buffer_size, batch_size):
-        self.device = device
-        self.memory = deque(maxlen=buffer_size)
-        self.batch_size = batch_size
-        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
-
-    def add(self, state, action, reward, next_state, done):
-        e = self.experience(state, action, reward, next_state, done)
-        self.memory.append(e)
-
-    def sample(self):
-        experiences = random.sample(self.memory, k=self.batch_size)
-
-        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(self.device)
-        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).long().to(self.device)
-        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(self.device)
-        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(self.device)
-        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(self.device)
-
-        return (states, actions, rewards, next_states, dones)
-
-    def __len__(self):
-        return len(self.memory)
+from src.replaybuffer import ReplayBuffer
 
 
 class Agent():
